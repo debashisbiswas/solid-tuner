8b6ffe72d24a43b981aa0c7e9740f2edad505175
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2023-06-14 21:40:56 -0700

    Toggle start/stop button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createSignal, Show } from "solid-js";
 import * as Pitchfinder from "pitchfinder";
 
 import styles from "./App.module.css";
 
-const noteStrings = [
+const FFT_SIZE = 4096;
+
+const NOTE_NAMES = [
     'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'
 ] as const;
 
@@ -13,40 +15,52 @@ function noteNameFromPitch(frequency: number | null): string | null {
     }
 
     const noteNum = 12 * (Math.log(frequency / 440) / Math.log(2));
-    return noteStrings[(Math.round(noteNum) + 69) % 12];
+    return NOTE_NAMES[(Math.round(noteNum) + 69) % 12];
 }
 
 const App: Component = () => {
     const [note, setNote] = createSignal<string | null>(null);
     const [pitch, setPitch] = createSignal<number | null>(null);
+    const [currentIntervalId, setCurrentIntervalId] = createSignal<number | null>(null);
 
     function startAudioProcessing() {
-        const fftSize = 4096;
         let audioContext = new window.AudioContext();
-        const detector = Pitchfinder.AMDF({
+        const pitchDetector = Pitchfinder.AMDF({
             sampleRate: audioContext.sampleRate
         });
 
-        let array = new Float32Array(fftSize);
-        let source: MediaStreamAudioSourceNode;
-        console.log(audioContext.sampleRate);
         let analyser = audioContext.createAnalyser();
-        analyser.fftSize = fftSize;
+        analyser.fftSize = FFT_SIZE;
 
+        let fftArray = new Float32Array(FFT_SIZE);
+        let source: MediaStreamAudioSourceNode;
         navigator.mediaDevices
             .getUserMedia({ audio: true, video: false })
             .then((stream) => {
                 source = audioContext.createMediaStreamSource(stream);
                 source.connect(analyser);
 
-                setInterval(() => {
-                    analyser.getFloatTimeDomainData(array);
-                    const frequency = detector(array);
-                    setPitch(frequency);
+                const intervalId = setInterval(() => {
+                    analyser.getFloatTimeDomainData(fftArray);
+                    const frequency = pitchDetector(fftArray);
                     const detectedNote = noteNameFromPitch(frequency);
+
+                    setPitch(frequency);
                     setNote(detectedNote);
                 }, 16);
+
+                setCurrentIntervalId(intervalId);
             });
+    }
+
+    function stopAudioProcessing() {
+        const id = currentIntervalId();
+        if (id !== null) {
+            clearInterval(id);
+            setPitch(null);
+            setNote(null);
+            setCurrentIntervalId(null);
+        }
     }
 
     return (
@@ -54,7 +68,12 @@ const App: Component = () => {
             <header class={styles.header}>
                 <p class={styles.link}>{note()}</p>
                 <p class={styles.link}>{pitch()}</p>
-                <button onClick={startAudioProcessing}>Start Tuning</button>
+                <Show
+                    when={currentIntervalId() === null}
+                    fallback={<button onClick={stopAudioProcessing}>Stop Tuning</button>}
+                >
+                    <button onClick={startAudioProcessing}>Start Tuning</button>
+                </Show>
             </header>
         </div>
     );
